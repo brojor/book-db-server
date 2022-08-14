0e7075ab34a07fa219a34c807dd5f0ac0bfb5c87
brojor <bronislav.jordan@gmail.com>
2022-08-14 10:29:54 +0200

    feat: add user credentials validation

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,12 +1,20 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class AuthController {
   public async register({ request, auth }: HttpContextContract) {
-    const email = request.input('email')
-    const password = request.input('password')
+    const userSchema = schema.create({
+      email: schema.string({ trim: true }, [
+        rules.email(),
+        rules.unique({ table: 'users', column: 'email', caseInsensitive: true }),
+      ]),
+      password: schema.string([rules.minLength(8)]),
+    })
 
-    const user = await User.create({ email, password })
+    const validCredentials = await request.validate({ schema: userSchema })
+    const user = await User.create(validCredentials)
+
     const { token } = await auth.login(user)
 
     return { token }
