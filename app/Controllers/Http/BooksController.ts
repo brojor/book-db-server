b83ab4956326dbc4ccc04bb82ecc671fef358145
brojor <bronislav.jordan@gmail.com>
2022-10-04 15:16:37 +0200

    refactor(BookController): remove unnecessary code

--- app/Controllers/Http/BooksController.ts
+++ app/Controllers/Http/BooksController.ts
@@ -2,21 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Book from 'App/Models/Book'
 
 export default class BooksController {
-  public async index({}: HttpContextContract) {}
-
-  public async create({}: HttpContextContract) {}
-
-  public async store({}: HttpContextContract) {}
-
   public async show({ params }: HttpContextContract) {
-    const book = await Book.findOrFail(params.id)
-
-    return book
+    return Book.findOrFail(params.id)
   }
-
-  public async edit({}: HttpContextContract) {}
-
-  public async update({}: HttpContextContract) {}
-
-  public async destroy({}: HttpContextContract) {}
 }
