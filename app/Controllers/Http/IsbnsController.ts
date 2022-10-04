37a980f73a445dd128c22d399644b32bf6a923fd
brojor <bronislav.jordan@gmail.com>
2022-10-04 14:28:56 +0200

    feat(IsbnController): first search for ISBN in local database

--- app/Controllers/Http/IsbnsController.ts
+++ app/Controllers/Http/IsbnsController.ts
@@ -1,10 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Book from 'App/Models/Book'
 import ISBNService from 'App/Services/ISBNService'
+
 // ošetřit pokud nenajde knihu
 
 export default class IsbnsController {
   public async index({ request }: HttpContextContract) {
     const isbn = request.param('isbn')
+    const book = await Book.findBy('isbn', isbn)
+    const author = await book?.related('author').query().first()
+
+    if (book) {
+      return { ...book.serialize(), author: author?.fullName }
+    }
     return ISBNService.getBookDetails(isbn)
   }
 }
